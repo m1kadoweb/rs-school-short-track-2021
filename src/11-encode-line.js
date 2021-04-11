e22a80b9165f0755dadebdd49b6877ebe323e86a
m1kadoweb <m1kadosgs@gmail.com>
2021-04-11 22:32:12 +0500

    feat: solve 11-encode-line.js task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const newArr = [];
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count++;
+    } else if (count === 1) {
+      newArr.push(arr[i]);
+    } else {
+      newArr.push(`${count}${arr[i]}`);
+      count = 1;
+    }
+  }
+  return newArr.join('');
 }
 
 module.exports = encodeLine;
